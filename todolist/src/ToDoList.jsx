00ab3440b19322969d01c3ed213f2c2b672fd125
Shital Patil <patilshitalbharat@gmail.com>
2025-10-20 12:46:01 +0530

    implemented mark done and delete feature

--- todolist/src/ToDoList.jsx
+++ todolist/src/ToDoList.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export function ToDoList() {
-  let [toDoArr, setToDoArr] = useState([{ task: "sample task", id: uuidv4() }]);
+  let [toDoArr, setToDoArr] = useState([
+    { task: "sample task", id: uuidv4(), isDone: false },
+  ]);
   let [newTask, setNewTask] = useState("");
 
   let updateToDoList = () => {
     setToDoArr((prevToDos) => {
-      return [...prevToDos, { task: newTask, id: uuidv4() }];
+      return [...prevToDos, { task: newTask, id: uuidv4(), isDone: false }];
     });
     setNewTask("");
   };
@@ -17,6 +19,23 @@ export function ToDoList() {
       return todo.id != id;
     });
     setToDoArr(newToDo);
+  };
+
+  let markAsDone = (id) => {
+    let newToDo = toDoArr.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isDone: true };
+      } else {
+        return todo;
+      }
+    });
+    setToDoArr(newToDo);
+  };
+
+  let markAllDone = () => {
+    setToDoArr((prevToDos) => {
+      return prevToDos.map((todo) => ({ ...todo, isDone: true }));
+    });
   };
 
   return (
@@ -34,12 +53,21 @@ export function ToDoList() {
         {toDoArr.map((todo) => {
           return (
             <li key={todo.id}>
-              {todo.task} &nbsp; &nbsp;
+              <span
+                style={todo.isDone ? { textDecoration: "line-through" } : {}}
+              >
+                {" "}
+                {todo.task}{" "}
+              </span>{" "}
+              &nbsp; &nbsp;
               <button onClick={() => deleteToDo(todo.id)}>Delete</button>
+              &nbsp;
+              <button onClick={() => markAsDone(todo.id)}>Mark As Done</button>
             </li>
           );
         })}
       </ul>
+      <button onClick={markAllDone}> Mark All task as Done</button>
     </div>
   );
 }
